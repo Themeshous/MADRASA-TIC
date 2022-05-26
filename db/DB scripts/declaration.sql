6a544df81f5aeeffb79cd5dffa35ce4b98c93131
Madjid-CH <a.chergui@esi-sba.dz>
2022-05-26 09:58:12 +0100

    get non rejected declarations by service is ready

--- db/DB scripts/declaration.sql
+++ db/DB scripts/declaration.sql
@@ -1,29 +1,23 @@
 CREATE TABLE `madrasatic`.`declarations`
 (
-    `id_dec`      INT          NOT NULL AUTO_INCREMENT,
-    `date`        VARCHAR(45)  NOT NULL,
-    `titre`       VARCHAR(100) NOT NULL,
-    `description` TEXT         NULL,
-    `image_path`       varchar(255)     NULL,
-    `emetteur`    VARCHAR(45)  NOT NULL,
-    `localisation` VARCHAR(45) NOT NULL,
-    `service` VARCHAR(50) NULL,
-    `type` VARCHAR(45) NOT NULL,
-    `etat` VARCHAR(45) NOT NULL,
-    `Supp` BOOLEAN  DEFAULT FALSE, /*faut ajouter ça pour nous aider a la suppression*/
-    `IDrap` INT NULL,
+    `id_dec`                   INT          NOT NULL AUTO_INCREMENT,
+    `date`                     VARCHAR(45)  NOT NULL,
+    `titre`                    VARCHAR(100) NOT NULL,
+    `description`              TEXT         NULL,
+    `image_path`               varchar(255) NULL,
+    `emetteur`                 VARCHAR(45)  NOT NULL,
+    `localisation`             VARCHAR(45)  NOT NULL,
+    `service`                  VARCHAR(50)  NULL,
+    `type`                     VARCHAR(45)  NOT NULL,
+    `etat`                     VARCHAR(45)  NOT NULL,
+    `Supp`                     BOOLEAN DEFAULT FALSE, /*faut ajouter ça pour nous aider a la suppression*/
+    `IDrap`                    INT          NULL,
+    `remarques_de_responsable` TEXT         NULL,
     PRIMARY KEY (`id_dec`),
     INDEX `declarer_idx` (`emetteur` ASC) VISIBLE,
     CONSTRAINT `declarer`
         FOREIGN KEY (`emetteur`)
             REFERENCES `madrasatic`.`users` (`Email`)
             ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    INDEX `attacher_idx` (`IDrap` ASC) VISIBLE,
-    CONSTRAINT `attacher`
-        FOREIGN KEY (`IDrap`)
-            REFERENCES `madrasatic`.`rapports` (`id_rap`)
-            ON DELETE NO ACTION
             ON UPDATE NO ACTION
-
 );
