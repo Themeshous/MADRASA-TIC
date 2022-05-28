39bbadd36168b1cfcbbd2394ba85e4991af27950
Madjid-CH <a.chergui@esi-sba.dz>
2022-05-28 15:56:40 +0100

    fetch archived

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {saveDeclarationToDB, fetchAllNonRejetedDeclarations} = require('../controllers/user-controller');
+const {saveDeclarationToDB, fetchAllNonRejetedDeclarations, fetchArchivedDeclarationsByEmail} = require('../controllers/user-controller');
 
 
 router.post('/saveDeclaration',  saveDeclarationToDB);
 router.get('/getDeclarations',  fetchAllNonRejetedDeclarations);
+router.get('/getDeclarations/archive', fetchArchivedDeclarationsByEmail);
 
 
 
